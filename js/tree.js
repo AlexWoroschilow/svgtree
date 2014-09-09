be5d402260cc8d891ca86ea1eb7eca5a258a8960
AlexWoroschilow <alex.woroschilow@gmail.com>
2014-09-09 16:21:48 +0200

    add logic to generate positions

--- js/tree.js
+++ js/tree.js
@@ -8,13 +8,37 @@ $(function () {
 
     svgDocument.bind('percent', function (event) {
 
+        var percent = event.percent;
+
+        var s = Snap("#svgbaum");
+
+        for (var i = 0; i < (percent / 1.5); i++) {
+
+            var x = 0;
+            while (x < 50 || x > 370) {
+                x = Math.floor((Math.random() * 250) + 1);
+            }
+
+            var y = 0;
+            while (y < 20 || y > 300) {
+                y = Math.floor((Math.random() * 220) + 1);
+            }
+
+            var bigCircle = s.circle(x, y, 12);
+            bigCircle.attr({
+                fill: "#006c00",
+                stroke: "#000000"
+            })
+        }
+
         console.info(event.percent);
     });
 
     svgDocument.trigger({
         type: 'percent',
-        percent: 12
+        percent: 68
     })
 
     console.info('Hallo, hier bin ich :)');
-});
+})
+;
